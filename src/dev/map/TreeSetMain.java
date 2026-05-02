fd025cbad533f5343bf6061d7c807e8be0dc0149
NuranM7 <nm0230058@gmail.com>
2026-05-02 22:26:53 +0400

    add: new files

---
+++ src/dev/map/TreeSetMain.java
@@ -0,0 +1,98 @@
+package dev.map;
+
+import java.util.*;
+
+public class TreeSetMain {
+
+    public static void main(String[] args) {
+
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> emails = ContactData.getData("email");
+
+//        NavigableSet<Contact> sorted = new TreeSet<>(phones);
+        Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
+        NavigableSet<Contact> sorted = new TreeSet<>(mySort);
+        sorted.addAll(phones);
+        sorted.forEach(System.out::println);
+
+        NavigableSet<String> justNames = new TreeSet<>();
+        phones.forEach(c -> justNames.add(c.getName()));
+        System.out.println(justNames);
+
+        NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
+        fullSet.addAll(emails);
+        fullSet.forEach(System.out::println);
+
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+        fullList.sort(sorted.comparator());
+        System.out.println("-".repeat(32));
+        fullList.forEach(System.out::println);
+
+        Contact min = Collections.min(fullSet, fullSet.comparator());
+        Contact max = Collections.max(fullSet, fullSet.comparator());
+
+        Contact first = fullSet.first();
+        Contact last = fullSet.last();
+
+        System.out.println("-".repeat(32));
+        System.out.printf("min = %s, first = %s %n", min.getName(), first.getName());
+        System.out.printf("max = %s, last = %s %n", max.getName(), last.getName());
+
+        NavigableSet<Contact> copiedSet = new TreeSet<>(fullSet);
+        System.out.println("First element = " + copiedSet.pollFirst());
+        System.out.println("Last element = " + copiedSet.pollLast());
+        copiedSet.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+
+        Contact daffy = new Contact("Daffy Duck");
+        Contact daisy = new Contact("Daisy Duck");
+        Contact snoopy = new Contact("Snoopy");
+        Contact archie = new Contact("Archie");
+
+        for (Contact c : List.of(daffy, daisy, last, snoopy)) {
+            System.out.printf("ceiling(%s)=%s%n",c.getName(),fullSet.ceiling(c));
+            System.out.printf("higher(%s)=%s%n",c.getName(),fullSet.higher(c));
+        }
+        System.out.println("-".repeat(32));
+
+        for (Contact c : List.of(daffy, daisy, first, archie)) {
+            System.out.printf("floor(%s)=%s%n",c.getName(),fullSet.floor(c));
+            System.out.printf("lower(%s)=%s%n",c.getName(),fullSet.lower(c));
+        }
+        System.out.println("-".repeat(32));
+
+        // return reversed Set
+        NavigableSet<Contact> descendingSet = fullSet.descendingSet();
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+
+        Contact lastConcat = descendingSet.pollLast();
+        System.out.println("Removed " + lastConcat);
+        descendingSet.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+        fullSet.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+
+        Contact marion = new Contact("Maid Marion");
+        var headSet = fullSet.headSet(marion);
+        headSet.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+
+        var tailSet = fullSet.tailSet(marion);
+        tailSet.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+
+        Contact linus = new Contact("Linus Van Pelt");
+        var subset = fullSet.subSet(linus, false, marion, true);
+        subset.forEach(System.out::println);
+        System.out.println("-".repeat(32));
+
+
+
+
+
+
+    }
+
+}
